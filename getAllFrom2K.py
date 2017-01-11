027ce818f7d24f799d483ef9c1204b670c706665
yellowkingdom <yellowkingdom@live.cn>
2017-01-11 21:41:56 +0800

    更新getAllFrom2K.py

---
+++ getAllFrom2K.py
@@ -0,0 +1,11 @@
+#coding:UTF-8
+import tushare as ts
+import sys
+today = date.today()
+
+# 如何使用: python -W ignore getAllFrom2K.py 002188
+# 结果将会保存在当前文件夹的xlsx里面,并用股票代码作为xlsx文件名
+
+for i in range(1, len(sys.argv)):
+	result = ts.get_h_data(sys.argv[i], start='2000-01-01', end=today.strftime("%Y-%m-%d"))
+	result.to_excel('./xlsx/'+sys.argv[i]+'.xlsx')
